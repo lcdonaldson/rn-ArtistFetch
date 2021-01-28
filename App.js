16f738e37b236e82836a8ccc8219d25c9f6256e9
Levi Donaldson <lcdbacanas@gmail.com>
2021-01-28 11:37:21 -0700

    adding components and starting to implement redux

--- App.js
+++ App.js
@@ -1,6 +1,7 @@
 
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 import axios from 'axios';
+import Artists from './src/components/Artists';
 import {
   SafeAreaView,
   StyleSheet,
@@ -8,36 +9,39 @@ import {
   Text,
   StatusBar,
   Pressable,
-  FlatList
+  FlatList,
+  ActivityIndicator
 } from 'react-native';
-
-import {
-  Header,
-  LearnMoreLinks,
-  Colors,
-  DebugInstructions,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
 
 export default class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      itemList: []
+      itemList: [],
+      isLoading: false
     };
   }
-  handleClick = () => {
-    let artist = 'Elvis'
-    axios.get(`http://itunes.apple.com/search?term=${artist}`)
+
+  handleClick = async () => {
+    this.setState({ isLoading: true });
+    let artist = 'Elvis';
+    await axios.get(`http://itunes.apple.com/search?term=${artist}`)
     .then(response => {
       this.setState({
         itemList: response.data.results
       });
     })
     .catch(error => console.log(error));
+    this.setState({ isLoading: false });
   }
+  
+  dateFormat = (time) => {
+    const DATE_OPTIONS = { weekday: 'short', year: 'numeric', month: 'short', day: 'numeric' };
+    let formattedTime = (new Date(time)).toLocaleDateString('en-US', DATE_OPTIONS)
+    return formattedTime
+  }
+
   render() {
-    {console.log('MYDATA', this.state.itemList)}
     return (
       <>
         <StatusBar barStyle="dark-content" />
@@ -45,31 +49,26 @@ export default class App extends React.Component {
           <View>
             <View>
               <Pressable 
-              style={styles.btnLayout} 
-              onPress={this.handleClick}
+                style={styles.btnLayout} 
+                onPress={this.handleClick}
               >
                 <Text style={styles.btnText}>Show Records</Text>
               </Pressable> 
             </View>
+            <ActivityIndicator animating={this.state.isLoading} size="large" color="red" />
             <View style={styles.listWrapper}>
               <FlatList
                 data={this.state.itemList}
-                
                 renderItem={({item}) => {
+                  let time = item.releaseDate
                   return (
-                    <View style={styles.infoWrapper}>
-                      <Text style={styles.primaryText}>{item.artistName}</Text>
-                      <View style={styles.row}>
-                        <View style={styles.flexLeft}>
-                          <Text style={styles.lText}>Title:  {item.trackName}</Text>
-                          <Text style={styles.lText}>Cost:  {item.trackPrice}</Text>
-                        </View>
-                        <View style={styles.flexRight}>
-                          <Text style={styles.rText}>Genre:  {item.primaryGenreName}</Text>
-                          <Text style={styles.rText}>Released on:  {item.releaseDate}</Text>
-                        </View>
-                      </View>
-                    </View>
+                    <Artists
+                      name={item.artistName}
+                      title={item.trackName}
+                      cost={item.trackPrice}
+                      genre={item.primaryGenreName}
+                      date={this.dateFormat(time)}
+                    />
                   )
                 }}
                 keyExtractor={item => item.trackId}
@@ -94,39 +93,6 @@ const styles = StyleSheet.create({
     padding: 12,
     marginTop: 25,
   },
-  infoWrapper: {
-    backgroundColor: 'lightblue',
-    borderColor: 'white',
-    borderWidth: 2,
-    flex: 1,
-    display: 'flex',
-    padding: 10
-  },
-  primaryText: {
-    fontSize: 30, 
-    color: '#000',
-    width: '100%',
-    display: 'flex',
-    borderBottomColor: 'teal',
-    borderBottomWidth: 2,
-    marginBottom: 10
-  },
-  row: {
-    display: 'flex',
-    flexDirection: 'row',
-  },
-  flexLeft: {
-    display: 'flex',
-    flexDirection: 'column',
-    width: '50%'
-  },
-  flexRight: {
-    display: 'flex',
-    flexDirection: 'column',
-    width: '50%'
-  },
-  lText: {paddingBottom: 10},
-  rText: {paddingBottom: 10},
   btnText: {
     fontSize: 20, 
     color: '#fff', 
